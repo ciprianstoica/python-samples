f158bad0d7cae1afd5255fbd6e7e10f8e0d99b0f
Ciprian Stoica <ciprian.stoica@orkas.ro>
2021-12-15 17:27:54 +0200

    Latest updates

---
+++ params.py
@@ -0,0 +1,9 @@
+# usage: run the following in command prompt (shell)
+# python params.py 'first parameter' 2nd parameter
+import sys
+
+# watch the content of sys.argv
+print(sys.argv)
+
+
+
---
+++ variables-in-func.py
@@ -0,0 +1,26 @@
+
+# guess the printed values of l1, l2, l3, l4, l5
+
+def func(a, b, c):
+    l1.append(6)
+    a += [6]
+    b = b + [6]
+    c.append(6)
+    l5 = [4, 5, 6]
+
+
+l1 = [4, 5]
+l2 = [4, 5]
+l3 = [4, 5]
+l4 = [4, 5]
+l5 = [4, 5]
+func(l2, l3, l4)
+
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(l5)
+
+
+
