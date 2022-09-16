ff1e1993dd582eeae43b52f15b0d8193e7bf1ac3
Ciprian Stoica <ciprianstoica75@gmail.com>
2022-09-16 13:33:33 +0300

    Added the presentation

--- geometry.py
+++ geometry.py
@@ -3,27 +3,24 @@ class WrongGeometry(Exception):
 
 
 class Point:
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # def __str__(self):
+    #     return f'Punctul de coordonate({self.x}, {self.y})'
+
     def __repr__(self):
         return f'Point({self.x}, {self.y})'
 
-    def __del__(self):
-        pass
-        # print('S-a distrus obiectul', self)
-
-    def __add__(self, other):
-        return Segment(self, other)
+    # def __del__(self):
+    #     print(f'distrus Point({self.x}, {self.y})')
 
     def get_distance(self):
         return round((self.x ** 2 + self.y ** 2) ** 0.5, 2)
 
 
 class Circle(Point):
-
     def __init__(self, x, y, r):
         super().__init__(x, y)
         self.r = r
@@ -32,28 +29,14 @@ class Circle(Point):
         return f'Circle({self.x}, {self.y}, {self.r})'
 
     def get_distance(self):
-        return round(super().get_distance() - self.r, 2)
+        return super().get_distance() - self.r
 
 
 class Segment:
 
     def __init__(self, p1: Point, p2: Point):
-        if type(p1) != Point:
-            raise WrongGeometry(f"Incorrect type. Only Point allowed. '{type(p1).__name__}' given.")
-        if type(p2) != Point:
-            raise WrongGeometry(f"Incorrect type. Only Point allowed. '{type(p2).__name__}' given.")
         self.p1 = p1
         self.p2 = p2
-
-    def __repr__(self):
-        # return f'Segment(Point({self.p1.x}, {self.p1.y}), Point({self.p2.x}, {self.p2.y}))'
-        return f'Segment({self.p1}, {self.p2})'
-
-    def __gt__(self, other):
-        return self.get_length() > other.get_length()
-
-    def __ge__(self, other):
-        return self.get_length() >= other.get_length()
 
     def __lt__(self, other):
         return self.get_length() < other.get_length()
@@ -61,100 +44,89 @@ class Segment:
     def __le__(self, other):
         return self.get_length() <= other.get_length()
 
+    def __gt__(self, other):
+        return self.get_length() > other.get_length()
+
+    def __ge__(self, other):
+        return self.get_length() >= other.get_length()
+
+    def __eq__(self, other):
+        return self.get_length() == other.get_length()
+
+    def __ne__(self, other):
+        return self.get_length() != other.get_length()
+
     def get_length(self):
-        return round((
-            (self.p1.x - self.p2.x) ** 2 + (self.p1.y - self.p2.y) ** 2) ** 0.5, 2)
-
-
-class Polyline:
-
-    def __init__(self, p1: Point, p2: Point, *points: Point):
-        self.__index = -1
-        self.points = [p1, p2, *points]
-        for p in self.points:
-            if type(p) != Point:
-                raise WrongGeometry(f"Incorrect type. Only Point allowed. '{type(p1).__name__}' given.")
-
-    def __repr__(self):
-        r = 'Polyline('
-        for p in self.points:
-            r += f'{p}, '
-        return r.rstrip(', ') + ')'
-
-    def __iter__(self):
-        self.__index = -1
-        return self
-
-    def __next__(self):
-        self.__index += 1
-        if self.__index == len(self.points):
-            raise StopIteration
-        return self.points[self.__index - 1]
-
-    def __len__(self):
-        return len(self.points)
-
-    def __getitem__(self, item):
-        return self.points[item]
+        return round(((self.p1.x - self.p2.x) ** 2 + (self.p1.y - self.p2.y) ** 2) ** 0.5, 2)
 
 
 class Rectangle:
 
     def __init__(self, p1: Point, p2: Point):
+        # if not isinstance(p1, Point) or not isinstance(p2, Point):
         if type(p1) != Point:
-            raise WrongGeometry(f"Incorrect type. Only Point allowed. '{type(p1).__name__}' given.")
+            raise WrongGeometry(f'Expected Point, got {type(p1).__name__}')
         if type(p2) != Point:
-            raise WrongGeometry(f"Incorrect type. Only Point allowed. '{type(p2).__name__}' given.")
+            raise WrongGeometry(f'Expected Point, got {type(p2).__name__}')
         self.p1 = p1
         self.p2 = p2
+        self.sides = [abs(self.p1.x - self.p2.x), abs(self.p1.y - self.p2.y)]
 
-    def get_dimensions(self):
-        """
-        Returns a list with the dimensions.
+    def __str__(self):
+        return f'Rectangle({self.p1}, {self.p2})'
+        # return f'Rectangle(Point({self.p1.x}, {self.p1.y}), Point({self.p2.x}, {self.p2.y}))'
 
-        Returns:
-            list: the list with the dimensions
+    def get_sides(self) -> list:
+        return self.sides
 
-        >>> r = Rectangle(Point(2, 3), Point(4, 6))
-        >>> r.get_dimensions()
-        [2, 3]
-        """
+    def get_area(self) -> float or int:
+        return self.sides[0] * self.sides[1]
 
-        return [abs(self.p1.x - self.p2.x), abs(self.p1.y - self.p2.y)]
+    def get_perimeter(self) -> float or int:
+        return (self.sides[0] + self.sides[1]) * 2
 
-    def get_area(self):
-        lat1, lat2 = self.get_dimensions()
-        return lat1 * lat2
+    def contains(self, c: Circle):
+        return (self.p1.x <= c.x + c.r <= self.p2.x or self.p2.x <= c.x + c.r <= self.p1.x or
+                self.p1.x <= c.x - c.r <= self.p2.x or self.p2.x <= c.x - c.r <= self.p1.x) and \
+               (self.p1.y <= c.y + c.r <= self.p2.y or self.p2.y <= c.y + c.r <= self.p1.y or
+                self.p1.y <= c.y - c.r <= self.p2.y or self.p2.y <= c.y - c.r <= self.p1.y)
 
-    def get_perimeter(self):
-        """
-        Gets the perimeter of the rectangle.
 
-        Returns:
-            float: the perimeter
+class Polyline:
 
-        >>> r = Rectangle(Point(2, 3), Point(4, 6))
-        >>> r.get_perimeter()
-        10
-        """
-        lat1, lat2 = self.get_dimensions()
-        return round((lat1 + lat2) * 2, 2)
+    def __init__(self, p1: Point, p2: Point, *points: Point):
+        self.points = (p1, p2, *points)
+        for p in self.points:
+            if type(p) != Point:
+                raise WrongGeometry(f'Expected Point, got {type(p).__name__}')
+
+    # def __init__(self, *points: Point):
+    #     if len(points) < 2:
+    #         raise WrongGeometry('Minimum 2 points are mandatory')
+    #     for p in points:
+    #         if type(p) != Point:
+    #             raise WrongGeometry(f'Expected Point, got {type(p).__name__}')
+    #     self.points = points
+
+    def __repr__(self):
+        ret = 'Polyline('
+        for p in self.points:
+            ret += f'{p}, '
+        ret = ret.rstrip(', ') + ')'
+        return ret
 
 
 if __name__ == '__main__':
-    from doctest import testmod
-    testmod()
+    # re = Rectangle(Point(3, 4), Point(-2, -6))
+    # re2 = Rectangle(Point(2, 3), Circle(2, 3, 5))
+    # print(re2)
+    # print(re.get_sides())
+    # print(re.get_area())
+    # print(re.get_perimeter())
+    # print(re)
+    #
+    # print(re.contains(Circle(1, -1, 2)))
 
-    poly = Polyline(Point(2, 3), Point(3, 4), Point(4, 5))
-
-    for p in poly:
-        print(p)
-
-    for i in range(len(poly)):
-        print(poly[i])
-
-
-
-
-
+    poli = Polyline(Point(2, 3), Point(4, 3), Point(2, 9), Point(-5, 0))
+    print(poli)
 
